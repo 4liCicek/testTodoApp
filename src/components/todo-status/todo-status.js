4005b2bdabbf55c23b4b4b854219d0b6a0126069
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-02-01 13:55:05 +0300

    Update todo-status.js

--- src/components/todo-status/todo-status.js
+++ src/components/todo-status/todo-status.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const TodoStatus = ({count}) => {
   return (
@@ -6,4 +6,4 @@ const TodoStatus = ({count}) => {
   )
 }
 
-export default TodoStatus
+export default TodoStatus;
