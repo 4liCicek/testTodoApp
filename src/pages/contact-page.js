9dae28f459c6219f3c58602a7e98ae5078f1f990
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-02-01 13:52:28 +0300

    Update contact-page.js

--- src/pages/contact-page.js
+++ src/pages/contact-page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const ContactPage = () => {
   return (
@@ -6,4 +6,4 @@ const ContactPage = () => {
   )
 }
 
-export default ContactPage
+export default ContactPage;
