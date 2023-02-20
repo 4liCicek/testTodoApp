0460888581964ece78b01d19f268ebaa037f0f8d
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-02-20 08:23:07 +0300

    Update comments-page.js

--- src/pages/comments-page.js
+++ src/pages/comments-page.js
@@ -1,12 +1,1 @@
-import React from 'react';
-import Comments from '../components/comments/comments';
 
-const CommentsPage = () => {
-  return (
-    <div>
-      <Comments/>
-    </div>
-  )
-}
-
-export default CommentsPage;
